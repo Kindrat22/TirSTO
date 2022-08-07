3d11bb7ea4f0d74e000f2a9bfe2b1171ec2ed420
oleksii02 <61844150+oleksii02@users.noreply.github.com>
2022-08-07 18:45:55 +0300

    зміни

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(5)
+
+  function  increment() {
+    setLikes(likes + 1)
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{likes}</h2>
+      <button onClick={increment}>IN</button>
+      <button onClick={decrement}>DEL</button>
     </div>
   );
 }
-
 export default App;
